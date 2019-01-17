dea736c35dd64aa586727448a91528e2922b9654
RM Clark <rodney@acst.com>
2019-01-16 20:02:01 -0500

    2nd session

--- ContosoSite/Models/ContosoModel.Designer.cs
+++ ContosoSite/Models/ContosoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\rodne\source\repos\ContosoSite\ContosoSite\Models\ContosoModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\rodne\Source\Repos\rodneyclark88\ContosoU\ContosoSite\Models\ContosoModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
